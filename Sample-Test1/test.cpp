85b2d3ebf003fe43bdea11c669623cebd773d085
jinwk.lee <jinwk.lee@samsung.com>
2024-05-22 13:35:03 +0900

    Create MockSSD

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,5 +1,14 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "../CRA_TeamProject_SSD/SSD.h"
+
+using namespace std;
+using namespace testing;
+
+class MockSSD : public SSD {
+	MOCK_METHOD(void, write, (int lba, string data), (override));
+	MOCK_METHOD(string, read, (int lba), (override));
+};
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
