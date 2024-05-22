fcb14e66312ee07cc22220e8736242cf830754f7
ljbsps <ljbsps@naver.com>
2024-05-22 14:11:02 +0900

    internal flush 구현  : Virtual SSD 소멸 시 cache data를 NAND에 저장

--- CRA_TeamProject_SSD/VirtualSSD.cpp
+++ CRA_TeamProject_SSD/VirtualSSD.cpp
@@ -11,3 +11,25 @@ std::string VirtualSSD::read(int lba)
 {
 	return std::string("temp");
 }
+
+VirtualSSD::~VirtualSSD()
+{
+	internalFlush();
+}
+
+void VirtualSSD::internalFlush()
+{
+	FILE* fp;
+	fopen_s(&fp, NAND_FILE_NAME, "w+");
+
+	if (fp == nullptr) {
+		throw exception_ptr();
+	}
+
+	for (map<int, int>::iterator it = cache.begin(); it != cache.end(); it++) {
+		string line = to_string((*it).first).append(",").append(to_string((*it).second));
+		fwrite(line.c_str(), sizeof(char), line.length(), fp);
+	}
+	fclose(fp);
+}
+
