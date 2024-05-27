bed50a1ee44f157cde0462f3ff1792bc8bf23fc7
jinwk.lee <oopit11@naver.com>
2024-05-27 14:53:36 +0900

    [refactoring] LogFileSize, makeBackupLogFileToCompressFile, makeLatestFileToBackupFile

--- CRA_TeamProject_SSD/Logger.cpp
+++ CRA_TeamProject_SSD/Logger.cpp
@@ -18,40 +18,35 @@ public:
 
 		if (LogFileSize(LATEST_LOG_NAME) < MAX_LOG_SIZE) return;
 
-		// until_*.log 파일을 찾아 .zip 파일로 변경
-		string prefix = "until_";
-		string old_extension = ".log";
-		string new_extension = ".zip";
-		for (const auto& entry : fs::directory_iterator(".")) {
-			if (entry.is_regular_file() && entry.path().filename().string().find(prefix) == 0 && entry.path().extension() == old_extension) {
-				fs::path new_path = entry.path().parent_path() / (prefix + entry.path().filename().string().substr(prefix.length(), entry.path().filename().string().length() - prefix.length() - old_extension.length()) + new_extension);
-				fs::rename(entry.path(), new_path);
-				cout << "Renamed: " << entry.path() << " -> " << new_path << endl;
-			}
-		}
+		makeBackupLogFileToCompressFile();
 
-		// 파일 크기가 maxSize를 초과하면 새 파일 이름 생성
+		makeLatestFileToBackupFile();
+	}
+
+	void makeLatestFileToBackupFile()
+	{
 		time_t now = time(nullptr);
 		struct tm timeinfo;
-		char buffer[80];
+		char NowTime[80];
 
 		time(&now);
 		localtime_s(&timeinfo, &now);
-		strftime(buffer, sizeof(buffer), "%y%m%d_%Hh_%Mm_%Ss", &timeinfo);
+		strftime(NowTime, sizeof(NowTime), "%y%m%d_%Hh_%Mm_%Ss", &timeinfo);
 
-		// 백업 파일 경로 생성
-		fs::path backupFilename = "until_" + string(buffer) + ".log"; // until_240527_10h_50m_11s.log
-
-		fs::rename("latest.log", backupFilename);
-
-		cout << "File renamed to: " << backupFilename << endl;
+		fs::rename(LATEST_LOG_NAME, LOG_PREFIX + string(NowTime) + LOG_EXTENSION);
 	}
 
-
+	void makeBackupLogFileToCompressFile()
+	{
+		for (const auto& entry : fs::directory_iterator(".")) {
+			if (!(entry.is_regular_file()) || entry.path().filename().string().find(LOG_PREFIX) != 0 && entry.path().extension() != LOG_EXTENSION)  continue;
+			
+			fs::rename(entry.path(), entry.path().parent_path() / (LOG_PREFIX + entry.path().filename().string().substr(LOG_PREFIX.length(), entry.path().filename().string().length() - LOG_PREFIX.length() - LOG_EXTENSION.length()) + COMPRESS_EXTENSION));
+		}
+	}
 
 	uintmax_t LogFileSize(string& LogName)
 	{
-
 		try {
 			fs::path latest_log_path = fs::current_path() / LogName;
 
@@ -62,13 +57,12 @@ public:
 					throw runtime_error("Failed to create 'latest.log' file.");
 				}
 				file.close();
-				cout << "'latest.log' file created." << endl;
 			}
 
 			return fs::file_size(latest_log_path);
 		}
 		catch (exception& e) {
-			throw("LogFileSize Check Error!!!")
+			throw("LogFileSize Check Error!!!");
 		}
 	}
 
@@ -77,6 +71,12 @@ private:
 	Logger& operator=(const Logger& other) = delete;
 	Logger(const Logger& other) = delete;
 
-	int MAX_LOG_SIZE = 10240;
+	const int MAX_LOG_SIZE = 10240;
+
 	string LATEST_LOG_NAME = "latest.log";
+
+	const string LOG_PREFIX = "until_";
+	const string LOG_EXTENSION = ".log";
+	const string COMPRESS_EXTENSION = ".zip";
+
 };
