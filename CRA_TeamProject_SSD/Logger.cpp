5df15e74c2e678e262e00ca9cdad357df0e7996b
jinwk.lee <oopit11@naver.com>
2024-05-27 14:28:06 +0900

    add LogFileSize()

--- CRA_TeamProject_SSD/Logger.cpp
+++ CRA_TeamProject_SSD/Logger.cpp
@@ -15,9 +15,45 @@ public:
 	}
 
 	void checkLogFile() {
-		uintmax_t latest_log_size = 0;
+
+		if (LogFileSize(LATEST_LOG_NAME) < MAX_LOG_SIZE) return;
+
+		// until_*.log 파일을 찾아 .zip 파일로 변경
+		string prefix = "until_";
+		string old_extension = ".log";
+		string new_extension = ".zip";
+		for (const auto& entry : fs::directory_iterator(".")) {
+			if (entry.is_regular_file() && entry.path().filename().string().find(prefix) == 0 && entry.path().extension() == old_extension) {
+				fs::path new_path = entry.path().parent_path() / (prefix + entry.path().filename().string().substr(prefix.length(), entry.path().filename().string().length() - prefix.length() - old_extension.length()) + new_extension);
+				fs::rename(entry.path(), new_path);
+				cout << "Renamed: " << entry.path() << " -> " << new_path << endl;
+			}
+		}
+
+		// 파일 크기가 maxSize를 초과하면 새 파일 이름 생성
+		time_t now = time(nullptr);
+		struct tm timeinfo;
+		char buffer[80];
+
+		time(&now);
+		localtime_s(&timeinfo, &now);
+		strftime(buffer, sizeof(buffer), "%y%m%d_%Hh_%Mm_%Ss", &timeinfo);
+
+		// 백업 파일 경로 생성
+		fs::path backupFilename = "until_" + string(buffer) + ".log"; // until_240527_10h_50m_11s.log
+
+		fs::rename("latest.log", backupFilename);
+
+		cout << "File renamed to: " << backupFilename << endl;
+	}
+
+
+
+	uintmax_t LogFileSize(string& LogName)
+	{
+
 		try {
-			fs::path latest_log_path = fs::current_path() / "latest.log";
+			fs::path latest_log_path = fs::current_path() / LogName;
 
 			if (!fs::exists(latest_log_path)) {
 
@@ -29,43 +65,11 @@ public:
 				cout << "'latest.log' file created." << endl;
 			}
 
-			latest_log_size = fs::file_size(latest_log_path);
+			return fs::file_size(latest_log_path);
 		}
 		catch (exception& e) {
-			throw(e.what());
-		}		
-
-		if (latest_log_size > MAX_LOG_SIZE) {
-
-			// until_*.log 파일을 찾아 .zip 파일로 변경
-			string prefix = "until_";
-			string old_extension = ".log";
-			string new_extension = ".zip";
-			for (const auto& entry : fs::directory_iterator(".")) {
-				if (entry.is_regular_file() && entry.path().filename().string().find(prefix) == 0 && entry.path().extension() == old_extension) {
-					fs::path new_path = entry.path().parent_path() / (prefix + entry.path().filename().string().substr(prefix.length(), entry.path().filename().string().length() - prefix.length() - old_extension.length()) + new_extension);
-					fs::rename(entry.path(), new_path);
-					cout << "Renamed: " << entry.path() << " -> " << new_path << endl;
-				}
-			}
-
-			// 파일 크기가 maxSize를 초과하면 새 파일 이름 생성
-			time_t now = time(nullptr);
-			struct tm timeinfo;
-			char buffer[80];
-
-			time(&now);
-			localtime_s(&timeinfo, &now);
-			strftime(buffer, sizeof(buffer), "%y%m%d_%Hh_%Mm_%Ss", &timeinfo);
-
-			// 백업 파일 경로 생성
-			fs::path backupFilename = "until_" + string(buffer) + ".log"; // until_240527_10h_50m_11s.log
-
-			fs::rename("latest.log", backupFilename);
-
-			cout << "File renamed to: " << backupFilename << endl;
+			throw("LogFileSize Check Error!!!")
 		}
-
 	}
 
 private:
@@ -74,4 +78,5 @@ private:
 	Logger(const Logger& other) = delete;
 
 	int MAX_LOG_SIZE = 10240;
+	string LATEST_LOG_NAME = "latest.log";
 };
